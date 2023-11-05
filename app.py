d06db0c386cdcbb4a0241fa5a30010aedd5e6aee
Shobhit150 <vshobhit150@gmail.com>
2023-11-05 23:55:12 +0530

    done

--- app.py
+++ app.py
@@ -8,27 +8,32 @@ from io import BytesIO
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/stock/*": {"origins": "*"}})
 
 @app.route('/stock/<symbol>/<start_date>/<end_date>')
 def stock_analysis(symbol, start_date, end_date):
-    # Download the historical stock price data
     stock_data = yf.download(symbol, start=start_date, end=end_date)
 
-    # Calculate daily returns
     stock_data['Daily Return'] = stock_data['Adj Close'].pct_change()
 
-    # Plot the adjusted closing price and save to a BytesIO object
-    plt.figure(figsize=(12, 6))
-    stock_data['Adj Close'].plot()
-    plt.title(f'{symbol} Adjusted Closing Price')
-    plt.xlabel('Date')
-    plt.ylabel('Price')
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 12))
+
+    stock_data['Adj Close'].plot(ax=ax1)
+    ax1.set_title(f'{symbol} Adjusted Closing Price')
+    ax1.set_xlabel('Date')
+    ax1.set_ylabel('Price')
+
+    stock_data['Daily Return'].plot(ax=ax2, color='green')
+    ax2.set_title(f'{symbol} Daily Returns')
+    ax2.set_xlabel('Date')
+    ax2.set_ylabel('Daily Return')
+
     plt.tight_layout()
+
     img = BytesIO()
     plt.savefig(img, format='png')
-    plt.close()
-    img.seek(0)  # go to the beginning of the BytesIO object
+    plt.close(fig)
+    img.seek(0) 
 
     return send_file(img, mimetype='image/png')
 
