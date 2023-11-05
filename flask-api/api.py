50a3dd6e8b465114de4937e43cb7b3f2af13f44c
Aditya Sahni <95126292+adityasahni7@users.noreply.github.com>
2023-11-05 17:59:36 +0530

    17:59

--- flask-api/api.py
+++ flask-api/api.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import yfinance as yf
 
 app = Flask(__name)
+CORS(app)
 
 @app.route('/analyze', methods=['POST'])
 def analyze_stock_data():
