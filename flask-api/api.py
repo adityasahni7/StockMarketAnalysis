397cb35c663f6cf42d771939ec023858902e32b3
Aditya Sahni <95126292+adityasahni7@users.noreply.github.com>
2023-11-05 17:57:43 +0530

    17:57

---
+++ flask-api/api.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+import yfinance as yf
+
+app = Flask(__name)
+
+@app.route('/analyze', methods=['POST'])
+def analyze_stock_data():
+    data = request.get_json()
+    stock_symbol = data['stock_symbol']
+    start_date = data['start_date']
+    end_date = data['end_date']
+
+    # In a real scenario, you would fetch stock data here using yfinance or another data source.
+    # For this example, we'll send a sample response.
+    sample_data = {
+        'stock_symbol': stock_symbol,
+        'start_date': start_date,
+        'end_date': end_date,
+        'sample_result': 'This is a sample response for stock analysis.',
+    }
+
+    return jsonify(sample_data)
+
+if __name__ == '__main__':
+    app.run()
---
+++ stock-analysis-app/src/App.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [stockSymbol, setStockSymbol] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [stockData, setStockData] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Make an API request to the backend here
+    try {
+      const response = await fetch('/get_stock_data', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ stock_symbol: stockSymbol, start_date: startDate, end_date: endDate }),
+      });
+      const data = await response.json();
+      setStockData(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Stock Price Analysis</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Stock Symbol:
+          <input type="text" value={stockSymbol} onChange={(e) => setStockSymbol(e.target.value)} required />
+        </label>
+        <br />
+        <label>
+          Start Date:
+          <input type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)} required />
+        </label>
+        <br />
+        <label>
+          End Date:
+          <input type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)} required />
+        </label>
+        <br />
+        <button type="submit">Analyze</button>
+      </form>
+      {stockData && (
+        <div>
+          <h2>Stock Data:</h2>
+          <pre>{JSON.stringify(stockData, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
+fetch('http://localhost:5000/analyze', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ stock_symbol: stockSymbol, start_date: startDate, end_date: endDate }),
+})
+.then(response => response.json())
+.then(data => {
+    // Handle the data from the Flask API
+});
